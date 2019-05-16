e530e58811d00aef1da38928462fa2aac0060236
D Hazelton <dphaz@bellsouth.net>
2019-05-16 19:26:15 -0400

    <2nd review corrections

--- js/main.js
+++ js/main.js
@@ -163,10 +163,10 @@ createRestaurantHTML = (restaurant) => {
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
-  image.setAttribute("alt", "Restraurant Picture");
+  image.setAttribute("alt", "restaurant.image");
   li.append(image);
 
-  const name = document.createElement('h1');
+  const name = document.createElement('h2');
   name.innerHTML = restaurant.name;
   name.tabIndex = '2';
   li.append(name);
